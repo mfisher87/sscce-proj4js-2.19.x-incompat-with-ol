b6ecac0ef14e19a487ca5f34bef85ae79b7aa8ba
Matt Fisher <mfisher87@gmail.com>
2025-06-19 12:41:04 -0600

    Init

---
+++ index.js
@@ -0,0 +1,3 @@
+import { register } from 'ol/proj/proj4.js';
+import proj4 from 'proj4';
+register(proj4.defs("EPSG:3413"));
---
+++ index.ts
@@ -0,0 +1,4 @@
+import { register } from 'ol/proj/proj4.js';
+import proj4 from 'proj4';
+
+register(proj4.defs("EPSG:3413"));
